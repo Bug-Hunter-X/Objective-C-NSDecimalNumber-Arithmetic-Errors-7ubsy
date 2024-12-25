3e6e3acb7234a9738727f1144d4280b9d027fa6c
BugHunter-X <askzenai@gmail.com>
2024-12-25 02:08:05 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,6 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Incorrect usage:
+        // NSDecimalNumber *num1 = [NSDecimalNumber decimalNumberWithString:@
